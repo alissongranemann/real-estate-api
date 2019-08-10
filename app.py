1f0999a8ecf0c53351796ec6cc0e1a64503cf2c3
Alisson Granemann Abreu <alissongranemannabreu@gmail.com>
2019-08-10 17:46:15 -0300

    new test route

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
     return "Hello World!"
 
-if __name__ == '__main__':
+
+@app.route("/<name>")
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+
+if __name__ == "__main__":
     app.run()
