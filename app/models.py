35e76189a21a274531dd90a9c3983607fe0746b8
Alisson Granemann Abreu <alissongranemannabreu@gmail.com>
2019-08-11 12:22:09 -0300

    add addresses migration

--- app/models.py
+++ app/models.py
@@ -1,20 +1,6 @@
 from app import db
 
 
-class Property(db.Model):
-    __tablename__ = "properties"
-
-    id = db.Column(db.Integer, primary_key=True)
-    area = db.Column(db.Integer(), nullable=False)
-    price = db.Column(db.Float(), nullable=False)
-    address_id = db.Column(db.Integer, db.ForeignKey("address.id"))
-
-    address = db.relationship("Address", back_populates="property")
-
-    def __repr__(self):
-        return f"<id {self.id}>"
-
-
 class Address(db.Model):
     __tablename__ = "addresses"
 
@@ -24,6 +10,20 @@ class Address(db.Model):
     cep = db.Column(db.String(9))
 
     property = db.relationship("Property", uselist=False, back_populates="address")
+
+    def __repr__(self):
+        return f"<id {self.id}>"
+
+
+class Property(db.Model):
+    __tablename__ = "properties"
+
+    id = db.Column(db.Integer, primary_key=True)
+    area = db.Column(db.Integer(), nullable=False)
+    price = db.Column(db.Float(), nullable=False)
+    address_id = db.Column(db.Integer, db.ForeignKey("addresses.id"))
+
+    address = db.relationship("Address", back_populates="property")
 
     def __repr__(self):
         return f"<id {self.id}>"
