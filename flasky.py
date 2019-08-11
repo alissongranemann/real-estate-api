1eb621b8a5eaa1d043f423a2a4bfc15656dada45
Alisson Granemann Abreu <alissongranemannabreu@gmail.com>
2019-08-11 12:24:15 -0300

    remove manage.py and update the requirements.txt

--- flasky.py
+++ flasky.py
@@ -6,10 +6,22 @@ dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
+import sys
+import click
+from flask_migrate import Migrate, upgrade
 from app import create_app, db
-app = create_app(os.environ["APP_SETTINGS"])
-
 from app.models import Property, Address
+
+app = create_app(os.environ["APP_SETTINGS"])
+migrate = Migrate(app, db)
+
+
+@app.cli.command()
+def deploy():
+    """Run deployment tasks."""
+    # migrate database to latest revision
+    upgrade()
+
 
 if __name__ == "__main__":
     app.run()
