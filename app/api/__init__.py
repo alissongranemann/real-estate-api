26d6046c1720091888843406f1d140bc9059aed9
Alisson Granemann Abreu <alissongranemannabreu@gmail.com>
2019-08-11 09:41:18 -0300

    rename views file

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,5 +4,5 @@ from flask_restful import Api
 api_blueprint = Blueprint("api", __name__)
 api = Api(api_blueprint)
 
-from .hello_world import HelloWorld
+from .views import HelloWorld
 api.add_resource(HelloWorld, "/")
