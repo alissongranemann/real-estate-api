0ae95a041f1f3b2e90d44d2d1d2fb015be3a4981
Alisson Granemann Abreu <alissongranemannabreu@gmail.com>
2019-08-11 17:05:38 -0300

    create manage.py for database operations

--- flasky.py
+++ flasky.py
@@ -6,21 +6,10 @@ dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-import sys
-import click
-from flask_migrate import Migrate, upgrade
 from app import create_app, db
 from app.models import Property, Address
 
 app = create_app(os.environ["APP_SETTINGS"])
-migrate = Migrate(app, db)
-
-
-@app.cli.command()
-def deploy():
-    """Run deployment tasks."""
-    # migrate database to latest revision
-    upgrade()
 
 
 if __name__ == "__main__":
