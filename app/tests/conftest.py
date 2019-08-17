a9fbca52d480119fa9b02f6352040c6113f956d7
Alisson Granemann Abreu <alissongranemannabreu@gmail.com>
2019-08-17 13:16:10 -0300

    Change tests config - Remove docker-compose setup - Create new database while executing the tests

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database, drop_database
 
 from app import create_app, db
 import config
@@ -7,11 +9,16 @@ import config
 @pytest.fixture
 def app():
     app = create_app(config.TestConfig)
+    database_url = app.config.get("SQLALCHEMY_DATABASE_URI")
+    engine = create_engine(database_url)
+    if not database_exists(engine.url):
+        create_database(engine.url)
     with app.app_context():
         db.create_all()
         yield app
         db.session.remove()
         db.drop_all()
+        drop_database(database_url)
 
 
 @pytest.fixture
