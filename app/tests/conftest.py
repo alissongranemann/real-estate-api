2a4d38a5d62f753ea29c5e7048ada941171b835d
Alisson Granemann Abreu <alissongranemannabreu@gmail.com>
2019-08-17 19:16:47 -0300

    adjust pytest fixtures to use session/function scope

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -2,25 +2,35 @@ import pytest
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database, drop_database
 
-from app import create_app, db
+from app import create_app
 import config
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def app():
     app = create_app(config.TestConfig)
+
     database_url = app.config.get("SQLALCHEMY_DATABASE_URI")
     engine = create_engine(database_url)
     if not database_exists(engine.url):
         create_database(engine.url)
+
+    yield app
+
+    drop_database(database_url)
+
+
+@pytest.fixture(scope="function")
+def db(app, request):
+    from app import db
+
     with app.app_context():
         db.create_all()
-        yield app
+        yield db
         db.session.remove()
         db.drop_all()
-        drop_database(database_url)
 
 
-@pytest.fixture
-def client(app):
+@pytest.fixture(scope="function")
+def client(app, db):
     return app.test_client()
--- app/tests/test_properties.py
+++ app/tests/test_properties.py
@@ -1,19 +1,18 @@
 import json
 
 
-def test_add_property(app):
-    with app.test_client() as client:
-        response = client.post(
-            "/api/v1/properties",
-            data=json.dumps(
-                {
-                    "area": 50,
-                    "price": 50000.0,
-                    "address": {"city": "City", "cep": "00000-000"},
-                }
-            ),
-            content_type="application/json",
-        )
-        data = json.loads(response.data.decode())
-        assert response.status_code == 201
-        assert "" == data
+def test_add_property(client):
+    response = client.post(
+        "/api/v1/properties",
+        data=json.dumps(
+            {
+                "area": 50,
+                "price": 50000.0,
+                "address": {"city": "City", "cep": "00000-000"},
+            }
+        ),
+        content_type="application/json",
+    )
+    data = json.loads(response.data.decode())
+    assert response.status_code == 201
+    assert "" == data
