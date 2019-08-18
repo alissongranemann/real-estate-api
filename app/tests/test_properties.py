227e5cd8b33d679bbc1748d37faf28f49b6db527
Alisson Granemann Abreu <alissongranemannabreu@gmail.com>
2019-08-18 08:20:49 -0300

    add more tests

--- app/tests/test_properties.py
+++ app/tests/test_properties.py
@@ -1,5 +1,7 @@
 import json
 
+from app.models import Property, Address
+
 
 def get_valid_property():
     return {
@@ -94,3 +96,54 @@ def test_add_invalid_city_property(client):
     assert city_error is not None
     assert len(city_error) == 1
     assert "required field" in city_error[0]
+
+
+def test_delete_property(client, db):
+    property = save_new_property(db)
+
+    response = client.delete(f"/api/v1/properties/{property.id}")
+    assert response.status_code == 204
+    assert Property.query.count() == 0
+
+
+def test_get_property(client, db):
+    property = save_new_property(db)
+
+    response = client.get(f"/api/v1/properties/{property.id}")
+    assert response.status_code == 200
+    data = json.loads(response.data.decode())
+    assert len(data) == 4
+    area = data.get("area")
+    assert area == 40
+    price = data.get("price")
+    assert price == 50000.0
+    address = data.get("address")
+    assert len(address) == 4
+    city = address.get("city")
+    assert city == "Florianopolis"
+    cep = address.get("cep")
+    assert cep == "00000-000"
+    neighbourhood = address.get("neighbourhood")
+    assert neighbourhood is None
+
+
+def test_get_properties(client, db):
+    save_new_property(db)
+    save_new_property(db)
+    save_new_property(db)
+
+    response = client.get(f"/api/v1/properties")
+    assert response.status_code == 200
+    data = json.loads(response.data.decode())
+    page = data.get("data")
+    assert len(page) == 3
+    total = data.get("total")
+    assert total == 3
+
+
+def save_new_property(db):
+    address = Address(city="Florianopolis", cep="00000-000")
+    property = Property(price=50000.0, area=40, address=address)
+    db.session.add(property)
+    db.session.commit()
+    return property
